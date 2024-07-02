b0a83125bcd8abb1be049721bd601cb9b50c8bf5
Ibitayo Akinnibosun <ibitayo.akinnibosun@gmail.com>
2024-07-02 20:54:56 +0100

    updated commit

--- app.js
+++ app.js
@@ -4,7 +4,7 @@ const PORT = 3000;
 
 const helloVisitorRouter = require("./helloVisitorRouter");
 
-app.use("/api/", helloVisitorRouter);
+app.use("/api", helloVisitorRouter);
 
 const start = app.listen(PORT, () => {
   console.log(`Server is listening on  http://localhost:${PORT}`);
