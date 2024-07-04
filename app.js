56a3991bcd5dd595de268986f5890e413d9c419a
Ibitayo Akinnibosun <ibitayo.akinnibosun@gmail.com>
2024-07-04 09:55:15 +0100

    updated commit

--- app.js
+++ app.js
@@ -1,7 +1,9 @@
 const express = require("express");
+const cors = require("cors");
+
 const app = express();
 const PORT = 3000;
-
+app.use(cors());
 const helloVisitorRouter = require("./helloVisitorRouter");
 
 app.use("/api", helloVisitorRouter);
